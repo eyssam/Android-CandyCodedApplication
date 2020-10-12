b399650e2916794c7b87bfd90f547b4177683920
Issam Bouhadida <issam.bouhadida@gmail.com>
2020-10-12 01:04:49 +0100

    Task 2 - Launch the Google Maps Activity_InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -25,11 +25,13 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-      public void createMapIntent(View view){
-        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando,FL 32801");
+    /*
+     ***
+     TODO - Task 2 - Launch the Google Maps Activity
+     ***
+    */
+    public void createMapIntent(View view){
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,uriAddress);
         mapIntent.setPackage("com.google.android.apps.maps");
 
@@ -37,7 +39,9 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
       }
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    /*
+     ***
+     TODO - Task 3 - Launch the Phone Activity
+     ***
+    */
 }
